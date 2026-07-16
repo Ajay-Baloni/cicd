9a7b5cdeb6c89132870f8714b8d6357efb57f16d
Ajay <ajaybaloni01@gmail.com>
2026-07-16 10:25:52 +0530

    ci cd test

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, CI/CD!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+
+if (require.main === module) {
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = server;
